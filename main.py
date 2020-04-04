91391fd03ad2706a8d03f52c8c5b2061e09d724c
esrev <gleb-petuhov@mail.ru>
2020-04-04 19:50:01 +0300

    added_start_handler

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(1, '/data')
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
 
 
@@ -8,11 +8,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-def main():
-    db_session.global_init("db/edu.sqlite")
-    session = db_session.create_session()
-    app.run()
+@app.route('/start')
+def start():
+    return render_template('main_page.html')
+
+
+
+# def main():
+#     db_session.global_init("db/edu.sqlite")
+#     session = db_session.create_session()
+#     app.run(port=8080, host='127.0.0.1')
 
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080, host='127.0.0.1')
