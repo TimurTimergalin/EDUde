7d233fd5755ec606a1e922df121aaa1c3be7b27b
TimurTimergalin <timur20041229@gmail.com>
2020-04-04 16:51:33 +0300

    teacher api added

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import sys
 sys.path.insert(1, '/data')
 from flask import Flask
 from data import db_session
+from flask_restful import Api
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+api = Api(app)
 
 
 def main():
---
+++ teacher_resources.py
@@ -0,0 +1,54 @@
+from flask import jsonify
+from flask_restful import reqparse, abort, Resource
+from data import db_session
+from data.teacher import Teacher
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('surname', required=True)
+parser.add_argument('name', required=True)
+parser.add_argument('password', required=True)
+parser.add_argument('email', required=True)
+
+
+def abort_if_teacher_not_found(teacher_id):
+    session = db_session.create_session()
+    teacher = session.query(Teacher).get(teacher_id)
+    if not teacher:
+        abort(404, message=f"News {teacher_id} not found")
+        return
+    return teacher
+
+
+class TeacherResource(Resource):
+    def get(self, teacher_id):
+        teacher = abort_if_teacher_not_found(teacher_id)
+        return jsonify({'teacher': teacher.to_dict(only=('surname', 'name'))})
+
+    def delete(self, teacher_id):
+        session = db_session.create_session()
+        teacher = abort_if_teacher_not_found(teacher_id)
+        session.delete(teacher)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class TeacherListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        teacher = session.query(Teacher).all()
+        return jsonify({'news': [item.to_dict(
+            only=('title', 'content', 'user.name')) for item in teacher]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        teacher = Teacher()
+        teacher.surname = args['surname']
+        teacher.name = args['name']
+        teacher.email = args['email']
+        teacher.set_password(args['password'])
+        session.add(teacher)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
