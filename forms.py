d476626f5177ebf4f6166379495112e6a237bd41
TimurTimergalin <timur20041229@gmail.com>
2020-04-06 15:23:05 +0300

    select to radio

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, RadioField, BooleanField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
@@ -12,7 +12,7 @@ class RegistrationForm(FlaskForm):
     email = EmailField('Эл. почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     check_password = PasswordField('Повторите пароль', validators=[DataRequired()])
-    user_type = SelectField('Учитель/Ученик', validators=[DataRequired()],
+    user_type = RadioField('Учитель/Ученик', validators=[DataRequired()],
                             choices=[('учитель', 'учитель'), ('ученик', 'ученик')])
     submit = SubmitField('Зарегестрироваться')
 
