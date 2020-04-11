80572018fe715e2b4c1517ba275c48cefd42c64b
TimurTimergalin <timur20041229@gmail.com>
2020-04-11 19:46:55 +0300

    recaptcha added

--- forms.py
+++ forms.py
@@ -2,6 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, RadioField, BooleanField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
+from flask_wtf.recaptcha import RecaptchaField
+
+
+RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
+RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
 
 
 class RegistrationForm(FlaskForm):
@@ -14,6 +19,7 @@ class RegistrationForm(FlaskForm):
     check_password = PasswordField('Повторите пароль', validators=[DataRequired()])
     user_type = RadioField('Учитель/Ученик', validators=[DataRequired()],
                            choices=[('учитель', 'Учитель'), ('ученик', 'Ученик')])
+    recaptcha = RecaptchaField()
     submit = SubmitField('Зарегистрироваться')
 
 
--- main.py
+++ main.py
@@ -35,6 +35,10 @@ api.add_resource(task_resources.TaskResource, '/api/task/<int:teacher_id>/<strin
 api.add_resource(task_resources.TaskListResource, '/api/task/<int:teacher_id>/<string:teacher_password>')
 
 
+RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
+RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
+
+
 @login_manager.user_loader
 def load_user(user_id):
     session = db_session.create_session()
