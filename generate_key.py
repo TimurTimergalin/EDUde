c3262723e0d5a1cc2b30f7fc23b7f086fb106ad7
TimurTimergalin <timur20041229@gmail.com>
2020-04-22 20:36:47 +0300

    student_api debug'ed

--- generate_key.py
+++ generate_key.py
@@ -2,5 +2,5 @@ import hashlib
 
 
 def generate_key(user):  # Teacher или Student, не User
-    # return hashlib.md5(f'{user.surname}{user.name}{user.hashed_password}'.encode('utf-8')).hexdigest()
-    return 'pass'
+    return hashlib.md5(f'{user.surname}{user.name}{user.hashed_password}'.encode('utf-8')).hexdigest()
+    # return 'pass
