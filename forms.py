cdf171a5bbdff7fd81aca446d3b2cfb38ebe9cad
kseniya <devksuksu@mail.ru>
2020-05-06 17:41:06 +0300

    доработки 1.3

--- forms.py
+++ forms.py
@@ -87,7 +87,7 @@ class EditClass(FlaskForm):
 class EditTask(FlaskForm):
     """EditTask
        WTF model of Edit task form"""
-    new_name = TextAreaField('Название задания', validators=[DataRequired()])
+    new_name = StringField('Название задания', validators=[DataRequired()])
     new_description = TextAreaField('Что делать', validators=[DataRequired()])
     new_deadline = DateTimeField("Дедлайн", validators=[DataRequired()], format='%Y-%m-%dT%H:%M')
     new_link = StringField('Куда отправлять', validators=[DataRequired()])
