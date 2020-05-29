0b3dfe3a67b2a943e7fd8e951ff31e92dcec3f62
DazaiOsamu <45696739+Daza10samu@users.noreply.github.com>
2020-05-29 20:43:12 +0300

    Try to fix student pass hash lenght

--- data/student.py
+++ data/student.py
@@ -21,7 +21,7 @@ class Student(SqlAlchemyBase, SerializerMixin, UserMixin):
     surname = Cl(sql.String(32))
     name = Cl(sql.String(32))
     email = Cl(sql.String(64), index=True, unique=True)
-    hashed_password = Cl(sql.String(64),  nullable=True)
+    hashed_password = Cl(sql.String(128),  nullable=True)
     teachers = orm.relationship('Teacher', secondary='teacher_to_student')
     class_rooms = orm.relationship('ClassRoom', secondary='student_to_class')
     users = orm.relation('User', back_populates='student')
